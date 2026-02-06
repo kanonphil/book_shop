ef04e82b81d68c15fc958f7824d0e1217b9315d9
kanonphil <xoghkdbwjd@gmail.com>
2026-02-06 11:10:31 +0900

    feat: Implement member CRUD operations
    - Add MemberMapper interface with databse queries
    - Create MemberService with business logic
    - Implement MemberController with REST endpoints

--- src/main/java/com/green/book_shop/member/controller/MemberController.java
+++ src/main/java/com/green/book_shop/member/controller/MemberController.java
@@ -1,9 +1,15 @@
 package com.green.book_shop.member.controller;
 
+import com.green.book_shop.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+  private final MemberService memberService;
 }
