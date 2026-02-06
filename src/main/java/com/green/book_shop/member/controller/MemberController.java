71f72cd3e1eb7b03d31f3cdf27cef1041dd14d92
kanonphil <xoghkdbwjd@gmail.com>
2026-02-06 11:04:45 +0900

    feat: Implement board DTO and MyBatis mapper

---
+++ src/main/java/com/green/book_shop/member/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.green.book_shop.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+}
